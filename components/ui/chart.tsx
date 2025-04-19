506d0f1757292d3994eeeba2db23015336e6b642
fabiendostie <fabien.dostie@gmail.com>
2025-04-19 04:53:30 -0400

    fix: add optional hide property to ChartConfig type

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -14,6 +14,7 @@ export type ChartConfig = {
   [k in string]: {
     label?: React.ReactNode;
     icon?: React.ComponentType;
+    hide?: boolean;
   } & (
     | { color?: string; theme?: never }
     | { color?: never; theme: Record<keyof typeof THEMES, string> }
